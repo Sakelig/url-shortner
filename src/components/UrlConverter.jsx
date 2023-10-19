7ef8fd5e7289a06e4115ebfa77705545b50ffd6f
dankon99 <daniel-kon@hotmail.com>
2023-10-19 10:30:47 +0200

    added listeners

--- src/components/UrlConverter.jsx
+++ src/components/UrlConverter.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react'
 import './UrlConverter.css'
 
 const UrlConverter = () => {
+  function handleSubmit() {
+    console.log("clicked")
+  }
+
+  function handleChange(e) {
+    console.log("changed")
+    setUrl(e.target.value)
+  }
+
+  const [url, setUrl] = useState('')
+
   return (
     <div>
        <label>
-        URL: <input name="myInput" />
+        URL: <input name="myInput" onChange={handleChange}/>
       </label>
-      <button>SHORTEN!</button>
-      <div className="output">Shortened URL</div>
+      <button onClick={handleSubmit}>SHORTEN!</button>
+      <div className="output">Shortened</div>
     </div>
   )
 }
